d2b3cea876cc0eaa94e7a6304a72d747a0ed8f31
Stuart Harris <stuart.harris@red-badger.com>
2023-10-13 18:08:37 +0100

    test deploy

--- crux-passkey-server/deploy.sh
+++ crux-passkey-server/deploy.sh
@@ -3,4 +3,7 @@
 # shellcheck source=.env
 source .env
 
-spin deploy --variable api_key="$SPIN_CONFIG_API_KEY" --sqlite @migration.sql
+spin cloud deploy --variable api_key="$SPIN_CONFIG_API_KEY"
+
+# note you'll need to substutite "generous-lion" with the name of your database instance
+spin cloud sqlite execute generous-lion "$(cat migration.sql)"
