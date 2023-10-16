653d0c84b4a8263edce67b466e0738daf1df5940
Stuart Harris <stuart.harris@red-badger.com>
2023-10-15 21:08:48 -0700

    insert or replace session

--- crux-passkey-server/webauthn/src/lib.rs
+++ crux-passkey-server/webauthn/src/lib.rs
@@ -1,16 +1,12 @@
 mod session;
 
-use std::str::FromStr;
-
 use anyhow::Result;
-use cookie::{Cookie, SameSite};
 use serde::{Deserialize, Serialize};
 use session::{Session, SessionId, SessionStore, SqliteSessionStore};
 use spin_sdk::{
     http::{Params, Request, Response, Router},
     http_component,
 };
-use uuid::Uuid;
 
 #[http_component]
 fn handle_request(req: Request) -> Result<Response> {
@@ -37,46 +33,42 @@ struct ToDo {
 
 fn register_start(_req: Request, _params: Params) -> Result<Response> {
     let mut session = Session::new();
-    let cookie = Cookie::build(("crux-passkey.register", session.id.to_string()))
-        .path("/auth")
-        .secure(true)
-        .same_site(SameSite::Strict)
-        .http_only(true)
-        .to_string();
     session.data = serde_json::to_vec(&ToDo {
         id: 1,
         description: "Do the thing".to_string(),
         due: "2021-01-01".to_string(),
     })?;
-    SqliteSessionStore::set(session)?;
+    SqliteSessionStore::set(&session)?;
+
     Ok(http::Response::builder()
-        .header("set-cookie", cookie)
+        .header("set-cookie", session.cookie("session_id", "/auth"))
         .status(200)
         .body(Some("yay!".into()))?)
 }
+
 fn register_finish(req: Request, _params: Params) -> Result<Response> {
-    let cookie = req
-        .headers()
-        .get("cookie")
-        .ok_or_else(|| anyhow::anyhow!("No cookie"))?;
-    let cookie = cookie::Cookie::parse(std::str::from_utf8(cookie.as_bytes())?)?;
-    let value = cookie.name_value().1;
-    println!("session_id: {:?}", value);
-    let session_id = SessionId(Uuid::from_str(value)?);
-    if let Some(session) = SqliteSessionStore::get(&session_id)? {
-        let todo: ToDo = serde_json::from_slice(&session.data)?;
-        Ok(http::Response::builder()
-            .status(200)
-            .body(Some(format!("{:?}", todo).into()))?)
-    } else {
-        Ok(http::Response::builder()
-            .status(404)
-            .body(Some("bla".into()))?)
-    }
+    let Some(session_id) = SessionId::from_request(&req)? else {
+        return Ok(http::Response::builder()
+            .status(400)
+            .body(Some("no session cookie".into()))?);
+    };
+
+    let Some(session) = SqliteSessionStore::get(&session_id)? else {
+        return Ok(http::Response::builder()
+            .status(400)
+            .body(Some("no session".into()))?);
+    };
+
+    let todo: ToDo = serde_json::from_slice(&session.data)?;
+    Ok(http::Response::builder()
+        .status(200)
+        .body(Some(format!("{:?}", todo).into()))?)
 }
+
 fn login_start(_req: Request, _params: Params) -> Result<Response> {
     todo!();
 }
+
 fn login_finish(_req: Request, _params: Params) -> Result<Response> {
     todo!();
 }
--- crux-passkey-server/webauthn/src/session.rs
+++ crux-passkey-server/webauthn/src/session.rs
@@ -1,11 +1,25 @@
-use std::fmt::Display;
+use std::{fmt::Display, str::FromStr};
 
 use anyhow::Result;
+use cookie::SameSite;
 use spin_sdk::sqlite::{Connection, ValueParam};
 use uuid::Uuid;
 
 #[derive(Clone, Debug)]
 pub struct SessionId(pub Uuid);
+
+impl SessionId {
+    pub fn from_request(req: &spin_sdk::http::Request) -> Result<Option<Self>> {
+        if let Some(cookie) = req.headers().get("cookie") {
+            let cookie = cookie::Cookie::parse(std::str::from_utf8(cookie.as_bytes())?)?;
+            let value = cookie.name_value().1;
+            let session_id = SessionId(Uuid::from_str(value)?);
+            Ok(Some(session_id))
+        } else {
+            Ok(None)
+        }
+    }
+}
 
 impl Display for SessionId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -26,11 +40,20 @@ impl Session {
             data: vec![],
         }
     }
+
+    pub fn cookie(&self, name: &str, path: &str) -> String {
+        cookie::Cookie::build((name, self.id.to_string()))
+            .path(path)
+            .secure(true)
+            .same_site(SameSite::Strict)
+            .http_only(true)
+            .to_string()
+    }
 }
 
 pub trait SessionStore {
     fn get(id: &SessionId) -> Result<Option<Session>>;
-    fn set(session: Session) -> Result<()>;
+    fn set(session: &Session) -> Result<()>;
     fn remove(id: &SessionId) -> Result<()>;
 }
 
@@ -59,14 +82,14 @@ impl SessionStore for SqliteSessionStore {
         }
     }
 
-    fn set(session: Session) -> Result<()> {
+    fn set(session: &Session) -> Result<()> {
         let connection = Connection::open_default()?;
         let execute_params = [
             ValueParam::Blob(session.id.0.as_bytes()),
             ValueParam::Blob(session.data.as_slice()),
         ];
         connection.execute(
-            "INSERT INTO user_session (id, data) VALUES (?1, ?2)",
+            "INSERT OR REPLACE INTO user_session (id, data) VALUES (?1, ?2)",
             execute_params.as_slice(),
         )?;
         Ok(())
